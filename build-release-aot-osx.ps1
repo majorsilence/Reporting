b05e2acb02967491d10f019b6db563eaf7d90e73
Peter Gill <peter@majorsilence.com>
2026-06-23 20:04:15 -0230

    add PdfNative AOT shared library with C API for Majorsilence.Pdf
    Covers the new PdfNative/ project (pdfnative.so/.dylib/.dll) exposing Majorsilence.Pdf and Majorsilence.Pdf.Security through a C-compatible FFI (document/canvas/style/table/merge + AES encryption), plus the three AOT build scripts and the pdfnative.h header.

--- build-release-aot-osx.ps1
+++ build-release-aot-osx.ps1
@@ -28,8 +28,12 @@ dotnet publish RdlCmd -c Release-DrawingCompat -r osx-arm64 -f $pTargetFramework
 dotnet publish RdlNative -c Release-DrawingCompat -r osx-x64   -f $pTargetFrameworkGeneric --self-contained true -p:PublishAot=true -p:GeneratePackageOnBuild=false -o "RdlNative/bin/$pConfigurationCompat/$pTargetFrameworkGeneric/osx-x64-aot/publish"
 dotnet publish RdlNative -c Release-DrawingCompat -r osx-arm64 -f $pTargetFrameworkGeneric --self-contained true -p:PublishAot=true -p:GeneratePackageOnBuild=false -o "RdlNative/bin/$pConfigurationCompat/$pTargetFrameworkGeneric/osx-arm64-aot/publish"
 
+dotnet publish PdfNative -c Release-DrawingCompat -r osx-x64   -f $pTargetFrameworkGeneric --self-contained true -p:PublishAot=true -p:GeneratePackageOnBuild=false -o "PdfNative/bin/$pConfigurationCompat/$pTargetFrameworkGeneric/osx-x64-aot/publish"
+dotnet publish PdfNative -c Release-DrawingCompat -r osx-arm64 -f $pTargetFrameworkGeneric --self-contained true -p:PublishAot=true -p:GeneratePackageOnBuild=false -o "PdfNative/bin/$pConfigurationCompat/$pTargetFrameworkGeneric/osx-arm64-aot/publish"
+
 $buildoutputpath_rdlcmd_aot = Join-Path $CURRENTPATH "Release-Builds" "build-output" "majorsilence-reporting-rdlcmd-aot"
 $buildoutputpath_rdlnative  = Join-Path $CURRENTPATH "Release-Builds" "build-output" "majorsilence-reporting-rdlnative"
+$buildoutputpath_pdfnative  = Join-Path $CURRENTPATH "Release-Builds" "build-output" "majorsilence-pdfnative"
 
 Remove-Item $buildoutputpath_rdlcmd_aot -Recurse -ErrorAction Ignore
 mkdir $buildoutputpath_rdlcmd_aot
@@ -55,10 +59,24 @@ Copy-Item (Join-Path $CURRENTPATH "RdlNative" "bin" $pConfigurationCompat $pTarg
 
 Copy-Item (Join-Path $CURRENTPATH "RdlNative" "rdlnative.h") (Join-Path $buildoutputpath_rdlnative "rdlnative.h")
 
+Remove-Item $buildoutputpath_pdfnative -Recurse -ErrorAction Ignore
+mkdir $buildoutputpath_pdfnative
+
+$pdfnative_osx_x64   = Join-Path $buildoutputpath_pdfnative "osx-x64"
+$pdfnative_osx_arm64 = Join-Path $buildoutputpath_pdfnative "osx-arm64"
+mkdir $pdfnative_osx_x64
+mkdir $pdfnative_osx_arm64
+
+Copy-Item (Join-Path $CURRENTPATH "PdfNative" "bin" $pConfigurationCompat $pTargetFrameworkGeneric "osx-x64-aot" "publish")   -Destination $pdfnative_osx_x64   -Recurse
+Copy-Item (Join-Path $CURRENTPATH "PdfNative" "bin" $pConfigurationCompat $pTargetFrameworkGeneric "osx-arm64-aot" "publish")  -Destination $pdfnative_osx_arm64  -Recurse
+
+Copy-Item (Join-Path $CURRENTPATH "PdfNative" "pdfnative.h") (Join-Path $buildoutputpath_pdfnative "pdfnative.h")
+
 $7zaExclude = "-xr!*.pdb", "-xr!*.dbg"
 $buildOutputDir = Join-Path $CURRENTPATH "Release-Builds" "build-output"
 
 Set-Location $buildOutputDir
 7z a -tzip "$Version-majorsilence-reporting-rdlcmd-aot-osx.zip"    @7zaExclude "majorsilence-reporting-rdlcmd-aot/"
 7z a -tzip "$Version-majorsilence-reporting-rdlnative-osx.zip"     @7zaExclude "majorsilence-reporting-rdlnative/"
+7z a -tzip "$Version-majorsilence-pdfnative-osx.zip"               @7zaExclude "majorsilence-pdfnative/"
 Set-Location $CURRENTPATH
